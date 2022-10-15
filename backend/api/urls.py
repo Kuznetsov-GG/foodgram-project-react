eaa35ee20c09e0c78d55ded02d779034336eeb1c
Григорий Кузнецов <grishik77788@gmail.com>
2022-10-15 22:22:09 +0500

    3

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,43 +10,24 @@ app_name = 'api'
 router = DefaultRouter()
 
 router.register('users', CreateUserView, basename='users')
-
 router.register('recipes', RecipeViewSet, basename='recipes')
-
 router.register('ingredients', IngredientViewSet, basename='ingredients')
-
 router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
-
     path('users/subscriptions/',
-
          SubscribeViewSet.as_view({'get': 'list'}), name='subscriptions'),
-
     path('users/<int:users_id>/subscribe/',
-
          SubscribeViewSet.as_view({'post': 'create',
-
                                    'delete': 'delete'}), name='subscribe'),
-
     path('recipes/<int:recipes_id>/shopping_cart/',
-
          ShoppingCartViewSet.as_view(
-
           {'post': 'create', 'delete': 'delete'}), name='shoppingcart'),
-
     path('recipes/<int:recipes_id>/favorite/',
-
          FavoriteViewSet.as_view({'post': 'create',
-
                                   'delete': 'delete'}), name='favorite'),
-
     path('recipes/download_shopping_cart/',
-
          DownloadShoppingCart.as_view({'get': 'download'}), name='download'),
-
     path('', include(router.urls)),
-
     path('auth/', include('djoser.urls.authtoken')),
-
 ]
