67d69195453918a0211f05dbb4cbb3e35b71d16b
Григорий Кузнецов <grishik77788@gmail.com>
2022-10-22 13:18:41 +0500

    Локально все работает

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = strtobool(os.getenv('DEBUG', default='False'))
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
